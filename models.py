c2c0e2e0049671e4268a7113d6b7c857d28925b5
Pio Luciano Losco <piolucianolosco@MBP-di-Pio.fritz.box>
2026-02-08 22:44:34 +0100

    increase password_hash length to 512

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
     role = db.Column(db.String(20), default="user")
     avatar = db.Column(db.String(120), nullable=True)  # 👈 NUOVO CAMPO
     last_seen_messages = db.Column(db.DateTime, nullable=True)
